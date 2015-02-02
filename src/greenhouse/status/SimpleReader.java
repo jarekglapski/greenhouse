ae10fe96111dfccf1d0eb18de49ac150677743d0
Jaroslaw_Glapski <jaroslaw_glapski@epam.com>
2015-02-02 23:44:25 +0100

    ReadWriteLock introduced + Post method in Spreadsheet logger

--- src/greenhouse/status/SimpleReader.java
+++ src/greenhouse/status/SimpleReader.java
@@ -4,6 +4,8 @@ import greenhouse.sensors.Sensor;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TimerTask;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  *
@@ -12,6 +14,10 @@ public class SimpleReader extends TimerTask implements Reader {
 
     private Set<Sensor> sensors;
 
+    private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
+
     private final Set<Measurement> lastMeasurement = new HashSet<>();
 
     public SimpleReader(Set<Sensor> sensors) {
@@ -19,20 +25,35 @@ public class SimpleReader extends TimerTask implements Reader {
     }
 
     @Override
-    public synchronized void run() {
-        lastMeasurement.clear();
-        for (Sensor sensor : sensors) {
-            store(Reader.getMeasurement(sensor));
+    public void run() {
+        writeLock.lock();
+        try {
+            lastMeasurement.clear();
+            for (Sensor sensor : sensors) {
+                store(Reader.getMeasurement(sensor));
+            }
+        } finally {
+            writeLock.unlock();
         }
     }
 
     protected void store(Measurement measurement) {
-        lastMeasurement.add(measurement);
+        writeLock.lock();
+        try {
+            lastMeasurement.add(measurement);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     @Override
-    public synchronized Set<Measurement> getLastMeasurement() {
-        return lastMeasurement;
+    public Set<Measurement> getLastMeasurement() {
+        readLock.lock();
+        try {
+            return lastMeasurement;
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
--- src/greenhouse/status/remote/BasicGoogleSpreadsheetPoster.java
+++ src/greenhouse/status/remote/BasicGoogleSpreadsheetPoster.java
@@ -8,10 +8,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Set;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.HttpsURLConnection;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 /**
  *
@@ -27,17 +31,21 @@ public class BasicGoogleSpreadsheetPoster extends TimerTask {
         this.sensorsReader = sensorsReader;
     }
 
-    private int sendPostRequest(String data) throws Exception {
+    /**
+     *
+     */
+    private int sendPostRequest(Set<Measurement> measurements) throws Exception {
+        String jsonData = getJSONData(measurements);
         URL endpoint = new URL(url);
         HttpsURLConnection connection = (HttpsURLConnection) endpoint.openConnection();
 
         //TODO: params as json? set req type etc!
         LOG.log(Level.FINE, "Sending POST request to URL: {0}", url);
-        LOG.log(Level.FINER, "Post parameters: {0}", data);
+        LOG.log(Level.FINER, "Post parameters: {0}", jsonData);
         connection.setRequestMethod("POST");
         connection.setDoOutput(true);
         try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-            wr.writeBytes(data);
+            wr.writeBytes(jsonData);
             wr.flush();
         }
         int responseCode = connection.getResponseCode();
@@ -49,12 +57,24 @@ public class BasicGoogleSpreadsheetPoster extends TimerTask {
         return responseCode;
     }
 
-    private int sendGetRequest(String data) throws ProtocolException, IOException {
-        URL endpoint = new URL(url + "?" + data);
+    private int sendGetRequest(Set<Measurement> measurements) throws ProtocolException, IOException {
+        int responseCode = 0;
+        for (Measurement measurement : measurements) {
+            responseCode = sendGetRequest(measurement);
+            if (responseCode != 200) {
+                break;
+            }
+        }
+        return responseCode;
+    }
+
+    private int sendGetRequest(Measurement measurement) throws ProtocolException, IOException {
+        String paramData = getData(measurement);
+        URL endpoint = new URL(url + "?" + paramData);
         HttpsURLConnection connection = (HttpsURLConnection) endpoint.openConnection();
 
         LOG.log(Level.FINE, "Sending GET request to URL: {0}", url);
-        LOG.log(Level.FINER, "Post parameters: {0}", data);
+        LOG.log(Level.FINER, "Post parameters: {0}", paramData);
         connection.setRequestMethod("GET");
         int responseCode = connection.getResponseCode();
 
@@ -80,13 +100,11 @@ public class BasicGoogleSpreadsheetPoster extends TimerTask {
 
     @Override
     public void run() {
-        sensorsReader.getLastMeasurement().stream().forEach((measurement) -> {
-            try {
-                sendGetRequest(getData(measurement));
-            } catch (Exception ex) {
-                Logger.getLogger(BasicGoogleSpreadsheetPoster.class.getName()).log(Level.SEVERE, "Cannot post measurement.", ex);
-            }
-        });
+        try {
+            sendPostRequest(sensorsReader.getLastMeasurement());
+        } catch (Exception ex) {
+            Logger.getLogger(BasicGoogleSpreadsheetPoster.class.getName()).log(Level.SEVERE, "Cannot post measurement.", ex);
+        }
     }
 
     public String getUrl() {
@@ -99,5 +117,28 @@ public class BasicGoogleSpreadsheetPoster extends TimerTask {
 
     private String getData(Measurement measurement) {
         return "datetime=test&temp1=100";
+    }
+
+    private String getJSONData(Measurement measurement) {
+        JSONObject object = new JSONObject();
+        object.put("datetime", measurement.getDate().getTime());
+        object.put(measurement.getSensorId(), measurement.getValue());
+        return object.toString();
+    }
+
+    /**
+     *
+     * @param measurements
+     * @return JSON array like:
+     * [{"datetime":"date0","temp1":"50"},{"datetime":"date1", "temp1":"60"}]
+     */
+    private String getJSONData(Set<Measurement> measurements) {
+        JSONStringer stringer = new JSONStringer();
+        stringer.array();
+        for (Measurement measurement : measurements) {
+            stringer.object().key("datetime").value(measurement.getDate().getTime()).key(measurement.getSensorId()).value(measurement.getValue()).endObject();
+        }
+        stringer.endArray();
+        return stringer.toString();
     }
 }
